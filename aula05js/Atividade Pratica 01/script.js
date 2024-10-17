588f90324e21f31143e07df8ac50323e58d7b097
Marcus Barreto <marcusbarreto@outlook.com.br>
2024-10-17 00:03:33 -0300

    Add files via upload

---
+++ aula05js/Atividade Pratica 01/script.js
@@ -0,0 +1,14 @@
+let animais = ["Cachorro", "Gato", "Papagaio"];
+animais.push("Tartaruga");
+
+animais.shift();
+
+animais.unshift("Coelho");
+
+animais[2] = "Hamster";
+
+console.log(animais.length);
+
+console.log(animais[1]);
+
+console.log(animais);
